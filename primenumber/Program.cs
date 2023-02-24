5e57e35d1e9a49fb461500dff819e2092afa79f0
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-24 12:07:24 +0530

    Add:logical

---
+++ primenumber/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace primenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check :");
+            int input = Convert.ToInt32(Console.ReadLine());
+            bool ifEnteredCheck = false;
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    Console.WriteLine("The number  is not a prime number");
+                    ifEnteredCheck = true;
+                    break;
+
+                }
+            }
+        }
+    }
+}
+
+
