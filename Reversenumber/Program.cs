4517b51785d2fa3d3bb0f70b6176729ac07ee75e
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-24 12:36:23 +0530

    Add:logical

---
+++ Reversenumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reversenumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number =654;
+
+            int revereNumber = 0;
+            int remainder = 0;
+            Console.WriteLine("Number before reverse = " + number);
+            while (number > 0)
+            {
+                remainder = number % 10;
+                number = number / 10;
+                revereNumber = revereNumber * 10 + remainder;
+            }
+            Console.WriteLine("Number after reverse  = " + revereNumber);
+        }
+    }
+}
+    
