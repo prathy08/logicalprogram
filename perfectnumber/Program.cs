769b4cd32ad938c308d4c6e50fb9c3360edb6cf5
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-24 11:42:41 +0530

    Add:logical

---
+++ perfectnumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace perfectnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int accumulator = 0;
+            for (int i= 1;i < input; i++)
+            {
+                if(input % i == 0)
+                {
+                    accumulator++;  
+                }
+            }
+            if (accumulator == input)
+            {
+                Console.WriteLine("The number "+input+"  is perfect");
+            }
+            else { Console.WriteLine("The number " + input + " is not perfect"); }
+
+
+        }
+    }
+}
